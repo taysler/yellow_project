abe9eccf3128a00d9477aba0afcd6b1c6813d86d
ChulMin Woo <talktowill.woo@gmail.com>
2021-05-13 17:04:50 +0900

    changing allowed host to all ALLOWED_HOSTS = ["*"]

--- yellow_project/settings.py
+++ yellow_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o38+2(va-4$3cj+*u!-n%41vird1pua53vcdmbo$vpt%v193gz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
